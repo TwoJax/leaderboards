7acabd29c14f246797d8ebee15087eb5ded0d310
Brandon Cordell <brandon@brandoncordell.com>
2024-08-06 15:37:58 -0400

    Hide link

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <router-view />
 
-  <footer class="absolute bottom-0 left-0">
+  <footer class="hidden absolute bottom-0 left-0">
     <router-link to="/time_entries/new" class="text-white" target="_blank">New Time Entries</router-link>
   </footer>
 </template>
